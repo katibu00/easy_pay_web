c6a44ea9105e5ac1054e21493a283abc1578dd13
katibu01 <ukmisau@gmail.com>
2023-10-25 22:17:19 +0100

    update

--- app/Http/Controllers/API/APIOrderController.php
+++ app/Http/Controllers/API/APIOrderController.php
@@ -34,7 +34,7 @@ class APIOrderController extends Controller
         // return response()->json(['message' => 'Order placed successfully'], 201);
         $userId = Auth::id();
 
-        $message = 'Order placed successfully. all: ' . $request->all();
+        $message = 'Order placed successfully. Address: ' . $request->address->addressType;
 
         return response()->json(['message' => $message], 201);
     
