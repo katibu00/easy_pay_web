0ec4d76c8bb458fbd2772c1bf5d2c86299cfabba
katibu01 <ukmisau@gmail.com>
2023-10-28 11:41:41 +0100

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -24,7 +24,6 @@ class Order extends Model
     }
     public function combo()
     {
-        return $this->belongsTo(Combo::class, 'combo_id');
+        return $this->belongsTo(Combo::class);
     }
-    
 }
